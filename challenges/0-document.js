b7ee23fae4710293ad840ceb520fc738c87fc6ec
TomOwen93 <mr.tsowen@gmail.com>
2023-06-15 15:49:52 +0100

    Implemented JS based on JSdoc documentation with tests included.

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,7 +1,18 @@
-// TODO (Ex.0): document and write tests for this function
-
+/**
+ * Given a number, returns the negative version of that number
+ * @param {number} n - input number
+ * @returns {number} 'n' as a negative
+ */
 function asNegative(n) {
-  // ...
+  let number = n;
+  if (n > 0) {
+    number = -n;
+  }
+
+  return number;
 }
 
-
+console.log(`asNegative(100):`, asNegative(100), `should be -100`);
+console.log(`asNegative(-110):`, asNegative(-110), `should be -110`);
+console.log(`asNegative(5000):`, asNegative(5000), `should be -5000`);
+console.log(`asNegative(50.55):`, asNegative(50.55), `should be -50.55`);
